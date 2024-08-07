4da9f685f1e929c2618403a90791a3f61b38fc34
Ahmed Khedr <94747792+Developer-Ahmed-Ayman-khedr@users.noreply.github.com>
2024-08-07 19:23:21 +0300

    Upload Port.h and Port.c drivers and update Dio.c and Dio.h.

---
+++ ClassicSourceCode/BSW/MCAL/Port/Port.c
@@ -0,0 +1,27 @@
+#include "Port.h"
+
+/*Initializes the Port Driver module.*/
+void Port_Init (const Port_ConfigType* ConfigPtr /*Pointer to configuration set.*/){
+    
+}
+
+/*Sets the port pin direction*/
+void Port_SetPinDirection (Port_PinType Pin/*Port Pin ID number*/, Port_PinDirectionType Direction/*Port Pin Direction*/){
+
+}
+
+/*Refreshes port direction.*/
+void Port_RefreshPortDirection (void){
+
+}
+
+/*Returns the version information of this module.*/
+void Port_GetVersionInfo (Std_VersionInfoType* versioninfo /*Pointer to where to store the version information of this module.*/){
+
+}
+
+/*Sets the port pin mode.*/
+void Port_SetPinMode (
+Port_PinType Pin /*Port Pin ID number*/, Port_PinModeType Mode /*New Port Pin mode to be set on port pin.*/){
+    
+}
